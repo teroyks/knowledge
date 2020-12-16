7231eeb080a2dec0e472cb28e87ae3ad1a4abe09
Tero Ykspetäjä <tero@tero.dev>
2020-12-16 14:50:04 +0200

    ANSI color codes for Bash

---
+++ programming/bash/term-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+echo -e "${BLACK}black${NC}"
+echo -e "${RED}red${NC}"
+echo -e "${GREEN}green${NC}"
+echo -e "${ORANGE}orange${NC}"
+echo -e "${BLUE}blue${NC}"
+echo -e "${PURPLE}purple${NC}"
+echo -e "${CYAN}cyan${NC}"
+echo -e "${LIGHT_GRAY}light_gray${NC}"
+echo -e "${DARK_GRAY}dark_gray${NC}"
+echo -e "${LIGHT_RED}light_red${NC}"
+echo -e "${LIGHT_GREEN}light_green${NC}"
+echo -e "${YELLOW}yellow${NC}"
+echo -e "${LIGHT_BLUE}light_blue${NC}"
+echo -e "${LIGHT_PURPLE}light_purple${NC}"
+echo -e "${LIGHT_CYAN}light_cyan${NC}"
+echo -e "${WHITE}white${NC}"
